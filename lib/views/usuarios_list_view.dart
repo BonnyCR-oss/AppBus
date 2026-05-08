f2c6333051d42bf618880fc12b8e01921fa21952
BenjaminVargas <vegeta.hacker.324@gmail.com>
2026-05-08 12:08:35 -0400

    todo lo anterior y un error de typeo

--- lib/views/usuarios_list_view.dart
+++ lib/views/usuarios_list_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import '../controllers/usuarios_controller.dart';
 import '../models/usuario_model.dart';
-// Importamos el nuevo archivo que acabas de crear
 import 'usuario_form_view.dart'; 
 
 class UsuariosListView extends StatefulWidget {
